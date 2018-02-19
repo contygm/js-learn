4309753cbe31e957880b2282b5522347eba903f6
Genevieve Conty <genevieve.conty@AV-MBP-7208.hitronhub.home>
2018-02-18 17:40:02 -0800

    setting up module pattern, jsdp 1.1

--- JSDP/section1/start/js/script.js
+++ JSDP/section1/start/js/script.js
@@ -1,4 +1,7 @@
-var leadself = 'Me: ',
+// --------------
+// MODULE PATTERN
+// --------------
+let leadself = 'Me: ',
 		leadcomputer = "PC: ",
 		aSaid= ["This is a Cyber Chat"],
 		msgYes = "Yes, that's a great idea.",
@@ -10,9 +13,39 @@ var leadself = 'Me: ',
 									"You’ve got about as much charm as a dead slug."];
 
 
+// NOTE: Methods are public
+// - can use in inspector console
+//
+function talk(msg) {
+	echo(leadself + " " + msg);
+}
 
+function replyYesNo() {
+	let msg = Math.random() > .5 ? msgYes : msgNo;
+	echo(leadcomputer + ": " + msg);
+}
 
+function saySassyStuff() {
+	// pick random sport in array -> randomNum < array.length
+	let msg = aSassyStuff[ Math.floor(Math.random() * aSassyStuff.length)];
 
+	echo(leadself + " " + msg);
+}
 
+function echo(msg) {
+	aSaid.push("<div>" + msg + "</div>");
 
+	// NOTE: putting things in variable lets you not have to dig into obj
+	// - ends up less processor intensive
+	//
+	let aSaidLength = aSaid.length;
+		start = Math.max(aSaidLength - 6, 0),
+		out = "";
 
+	for(var i = start; i < aSaid.length; i++) {
+		out += aSaid[i];
+	}
+
+	$('.advert').html(out);
+	$('#talk span').text(msg);
+}
